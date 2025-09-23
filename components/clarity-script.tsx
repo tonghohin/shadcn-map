1caca9c5c2b711da8efb9e480396329adbf008c6
Hin <tonghohin77@gmail.com>
2025-09-23 12:34:56 -0400

    fix: clarity script

--- components/clarity-script.tsx
+++ components/clarity-script.tsx
@@ -5,9 +5,9 @@ import { useEffect } from "react"
 
 export function ClarityScript() {
     const clarityProjectId = process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID
-    if (!clarityProjectId) return null
 
     useEffect(() => {
+        if (!clarityProjectId) return
         if (window.location.hostname === "localhost") return
 
         Clarity.init(clarityProjectId)
