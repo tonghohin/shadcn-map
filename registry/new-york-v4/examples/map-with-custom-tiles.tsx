2c7f7fb003bce4bbc3540aee9794c7fb1b832e0a
Hin <tonghohin77@gmail.com>
2025-09-26 18:13:58 -0400

    fix: tile error

--- registry/new-york-v4/examples/map-with-custom-tiles.tsx
+++ registry/new-york-v4/examples/map-with-custom-tiles.tsx
@@ -9,8 +9,6 @@ export function MapWithCustomTiles() {
             <MapTileLayer
                 url="https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}"
                 attribution="Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC"
-                darkUrl="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
-                darkAttribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             />
         </Map>
     )
