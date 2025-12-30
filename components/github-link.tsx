d279be967a7d10ec3ea2e2f505fda9466f23e760
Hin <tonghohin77@gmail.com>
2025-12-29 21:58:42 -0500

    docs: fix

--- components/github-link.tsx
+++ components/github-link.tsx
@@ -25,7 +25,7 @@ export function GitHubLink() {
 export async function StarsCount() {
     const data = await fetch(
         "https://api.github.com/repos/tonghohin/shadcn-map",
-        { cache: "no-store" }
+        { next: { revalidate: 0 } }
     )
     const json = await data.json()
     if (!json.stargazers_count) return null
